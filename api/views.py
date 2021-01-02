5b94d899d629440aed78c08448d95940237dad03
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-02 18:57:14 +0100

    add django decorators for apiview

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # Create your views here.
+@api_view(['GET'])
 def apiOverview(request):
 
 
