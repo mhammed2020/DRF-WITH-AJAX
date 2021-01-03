1bd949bd15bab60b29890a3e3c8606604846b4c6
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-03 09:40:42 +0100

    create taskDelete function

--- api/views.py
+++ api/views.py
@@ -57,6 +57,14 @@ def taskUpdate(request, pk):
 	return Response(serializer.data)
 
 
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+	task = Task.objects.get(id=pk)
+	task.delete()
+
+	return Response('Item succsesfully delete!')
+
+
     
 
 
