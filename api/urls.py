ad4fe752b1e4ce82337a4a98e9cd581a7b005f9c
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-03 09:34:34 +0100

    update url pattern

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
 	path('', views.apiOverview, name="api-overview"),
 	path('task-list/', views.taskList, name="task-list"),
 	path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
-		path('task-create/', views.taskCreate, name="task-create"),
+	path('task-create/', views.taskCreate, name="task-create"),
+	path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
 
 
 
