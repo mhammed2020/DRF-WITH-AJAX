5e023f1cb8011ec4f80451917a00a96771747846
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-01-03 09:33:45 +0100

    create taskUpdate

--- api/views.py
+++ api/views.py
@@ -44,6 +44,16 @@ def taskCreate(request):
 		serializer.save()
 
 	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+	task = Task.objects.get(id=pk)
+	serializer = TaskSerializer(instance=task, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
     
 
 
